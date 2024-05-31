0e74683e463eb47e4d038fd020c3e74e8a11da96
Alessia <alessia.claudia13@gmail.com>
2024-05-31 17:52:42 +0300

    functii2

---
+++ alessia/functii2.py
@@ -0,0 +1,30 @@
+#Functie pentru produs la un numar variabil de argumente
+
+def produs(*args):
+    p=1
+    for i in args:
+        p*=i
+    return p
+
+print(produs(1,2,3,4,5))
+
+#Functie pentru a afisa o lista de numere
+def numere(v):
+    for i in range(9):
+        v.append(i)
+    return v
+v=[]
+print(numere(v))
+
+
+#Functie pentru a afisa o lista de dictionare
+
+print("dictionar roman-englez")
+print("dictionar explicativ al limbii romane")
+print("dictionar roman-francez")
+
+people=[
+   {"name":"saulea","age":"69", "city":"braila"},]
+print(people)
+for p in people:
+   print(p)
